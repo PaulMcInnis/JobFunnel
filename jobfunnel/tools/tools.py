2cc67bd2384bf727e2a3c920612b0c4216ef6b09
bunsenmurder <ErichMengore@gmail.com>
2019-11-14 02:16:34 -0500

    This commit closes #13 and closes #12
    - Rewrote regex quantifiers to achieve faster performance and account for multiple cases (7 days or 7d).
    - Cut down or re-arranged conditional statements to improve performance and readability.

--- jobfunnel/tools/tools.py
+++ jobfunnel/tools/tools.py
@@ -22,23 +22,23 @@ def post_date_from_relative_post_age(job):
 
     post_date = None
 
-    # try known phrases like 7 hours ago or 3 days ago
+    # Supports almost all formats like 7 hours|days and 7 hr|d|+d
     try:
         # hours old
-        hours_ago = re.findall(r'(\d+)[0-9]*.*hour.*ago', job['date'])[
+        hours_ago = re.findall(r'(\d+)(?:[ +]{1,3})?(?:hour|hr)', job['date'])[
             0]
         post_date = datetime.now() - timedelta(hours=int(hours_ago))
     except IndexError:
         # days old
         try:
             days_ago = \
-                re.findall(r'(\d+)[0-9]*.*day.*ago', job['date'])[0]
+                re.findall(r'(\d+)(?:[ +]{1,3})?(?:day|d)', job['date'])[0]
             post_date = datetime.now() - timedelta(days=int(days_ago))
         except IndexError:
             # months old
             try:
                 months_ago = \
-                    re.findall(r'(\d+)[0-9]*.*month.*ago', job['date'])[
+                    re.findall(r'(\d+)(?:[ +]{1,3})?month', job['date'])[
                         0]
                 post_date = datetime.now() - relativedelta(
                     months=int(months_ago))
@@ -46,7 +46,7 @@ def post_date_from_relative_post_age(job):
                 # years old
                 try:
                     years_ago = \
-                        re.findall(r'(\d+)[0-9]*.*year.*ago',
+                        re.findall(r'(\d+)(?:[ +]{1,3})?year',
                                    job['date'])[
                             0]
                     post_date = datetime.now() - relativedelta(
@@ -54,31 +54,18 @@ def post_date_from_relative_post_age(job):
                 except:
                     pass
 
-    # try phrases like today or yesterday
-    if re.findall(r'[tT]oday', job['date']) and not post_date:
+    # try phrases like today, just posted, or yesterday
+    if re.findall(r'[tT]oday|[jJ]ust [pP]osted', job['date']) and not post_date:
         # today
         post_date = datetime.now()
     elif re.findall(r'[yY]esterday', job['date']):
         # yesterday
         post_date = datetime.now() - timedelta(days=int(1))
-
-    # try phrases like 1 d or 2 d or 30d+
-    if re.findall(r'[0-9]* *d.*', job['date']) and not post_date:
-        # some time in the past
-        post_date = datetime.now() - timedelta(
-            days=int(re.sub('d.*', '', job['date'])))
-
-    # try phrases like 24 hr
-    if re.findall(r'[0-9]* *hr.*', job['date']) and not post_date:
-        # some time in the past
-        post_date = datetime.now() - timedelta(
-            hours=int(re.sub('hr.*', '', job['date'])))
-
-    if not post_date:
+    elif not post_date:
         # must be from the 1970's
         post_date = datetime(1970, 1, 1)
         logging.error(
             'unknown date for job {}'.format(job['id']))
 
-    job['date'] = post_date.strftime('%d, %b %Y')
+    job['date'] = post_date.strftime('%Y-%m-%d')
     return job['date']
