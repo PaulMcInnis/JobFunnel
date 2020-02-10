20f7596adecb3c7ab7ed06f074af1a63f214d72b
Riya Agrahari <500052961@stu.upes.ac.in>
2020-02-10 12:37:19 +0530

    minor updates

--- jobfunnel/lambda_function.py
+++ jobfunnel/lambda_function.py
@@ -13,6 +13,12 @@ from country_hash import *
 from config import *
 import pandas as pd 
 from global_cache import * 
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 PROVIDERS = {'indeed': Indeed, 'monster': Monster, 'glassdoor': GlassDoor}
@@ -119,7 +125,7 @@ def lambda_handler(event,context):
                 provider: Union[GlassDoor, Monster, Indeed] = PROVIDERS[p](config)
                 provider_id = provider.__class__.__name__
                 try:
-                    #print('hi')
+                    #print('h
                     provider.scrape()
                     #print('hi2')
                     jf.scrape_data.update(provider.scrape_data)
@@ -141,6 +147,39 @@ def lambda_handler(event,context):
 '''s3 = boto3.client('s3')
 s3.upload_file(config['master_list_path'], S3_BUCKET_NAME, 'master_list.csv')
 os.system("ls /tmp/")'''
+
+creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+if os.path.exists(''):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
 
     
 lambda_handler(1,1)
