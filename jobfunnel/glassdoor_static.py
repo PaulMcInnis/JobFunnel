dfb858ca6e32888799edb3e5be9aab8dab8751a7
Arax1 <anandraju55@hotmail.com>
2020-06-13 18:44:40 -0400

    fixed settings and quotations

--- jobfunnel/glassdoor_static.py
+++ jobfunnel/glassdoor_static.py
@@ -16,83 +16,83 @@ from .glassdoor_base import GlassDoorBase
 class GlassDoorStatic(GlassDoorBase):
     def __init__(self, args):
         super().__init__(args)
-        self.provider = "glassdoorstatic"
+        self.provider = 'glassdoorstatic'
         self.headers = {
-            "accept": "text/html,application/xhtml+xml,application/xml;"
-            "q=0.9,image/webp,*/*;q=0.8",
-            "accept-encoding": "gzip, deflate, sdch, br",
-            "accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
-            "referer": "https://www.glassdoor.{0}/".format(
-                self.search_terms["region"]["domain"]
+            'accept': 'text/html,application/xhtml+xml,application/xml;'
+            'q=0.9,image/webp,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, sdch, br',
+            'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
+            'referer': 'https://www.glassdoor.{0}/'.format(
+                self.search_terms['region']['domain']
             ),
-            "upgrade-insecure-requests": "1",
-            "user-agent": self.user_agent,
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
+            'upgrade-insecure-requests': '1',
+            'user-agent': self.user_agent,
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
         }
 
-    def get_search_url(self, method="get"):
+    def get_search_url(self, method='get'):
         """gets the glassdoor search url"""
         # form the location lookup request data
-        data = {"term": self.search_terms["region"]
-                ["city"], "maxLocationsToReturn": 10}
+        data = {'term': self.search_terms['region']
+                ['city'], 'maxLocationsToReturn': 10}
 
         # form the location lookup url
-        location_url = "https://www.glassdoor.co.in/findPopularLocationAjax.htm?"
+        location_url = 'https://www.glassdoor.co.in/findPopularLocationAjax.htm?'
 
         # get the location id for search location
         location_response = self.s.post(
             location_url, headers=self.location_headers, data=data
         ).json()
 
-        if method == "get":
+        if method == 'get':
             # @TODO implement get style for glassdoor
             raise NotImplementedError()
-        elif method == "post":
+        elif method == 'post':
             # form the job search url
             search = (
-                f"https://www.glassdoor."
+                f'https://www.glassdoor.'
                 f"{self.search_terms['region']['domain']}/Job/jobs.htm"
             )
 
             # form the job search data
             data = {
-                "clickSource": "searchBtn",
-                "sc.keyword": self.query,
-                "locT": "C",
-                "locId": location_response[0]["locationId"],
-                "jobType": "",
-                "radius": self.convert_radius(self.search_terms["region"]["radius"]),
+                'clickSource': 'searchBtn',
+                'sc.keyword': self.query,
+                'locT': 'C',
+                'locId': location_response[0]['locationId'],
+                'jobType': '',
+                'radius': self.convert_radius(self.search_terms['region']['radius']),
             }
 
             return search, data
         else:
-            raise ValueError(f"No html method {method} exists")
+            raise ValueError(f'No html method {method} exists')
 
     def search_page_for_job_soups(self, data, page, url, job_soup_list):
         """function that scrapes the glassdoor page for a list of job soups"""
-        log_info(f"getting glassdoor page {page} : {url}")
+        log_info(f'getting glassdoor page {page} : {url}')
 
         job = BeautifulSoup(
             self.s.post(url, headers=self.headers,
                         data=data).text, self.bs4_parser
-        ).find_all("li", attrs={"class", "jl"})
+        ).find_all('li', attrs={'class', 'jl'})
         job_soup_list.extend(job)
 
     def search_joblink_for_blurb(self, job):
         """function that scrapes the glassdoor job link for the blurb"""
-        search = job["link"]
-        log_info(f"getting glassdoor search: {search}")
+        search = job['link']
+        log_info(f'getting glassdoor search: {search}')
         job_link_soup = BeautifulSoup(
             self.s.post(
                 search, headers=self.location_headers).text, self.bs4_parser
         )
 
         try:
-            job["blurb"] = job_link_soup.find(
-                id="JobDescriptionContainer").text.strip()
+            job['blurb'] = job_link_soup.find(
+                id='JobDescriptionContainer').text.strip()
         except AttributeError:
-            job["blurb"] = ""
+            job['blurb'] = ''
 
         filter_non_printables(job)
 
@@ -102,18 +102,18 @@ class GlassDoorStatic(GlassDoorBase):
         """gets blurb from glassdoor job link and sets delays for requests"""
         sleep(delay)
 
-        search = job["link"]
-        log_info(f"delay of {delay:.2f}s, getting glassdoor search: {search}")
+        search = job['link']
+        log_info(f'delay of {delay:.2f}s, getting glassdoor search: {search}')
 
         res = self.s.post(search, headers=self.location_headers).text
         return job, res
 
     def scrape(self):
         """function that scrapes job posting from glassdoor and pickles it"""
-        log_info(f"jobfunnel glassdoor to pickle running @ {self.date_string}")
+        log_info(f'jobfunnel glassdoor to pickle running @ {self.date_string}')
 
         # get the search url and data
-        search, data = self.get_search_url(method="post")
+        search, data = self.get_search_url(method='post')
 
         # get the html data, initialize bs4 with lxml
         request_html = self.s.post(search, headers=self.headers, data=data)
@@ -123,10 +123,10 @@ class GlassDoorStatic(GlassDoorBase):
 
         # scrape total number of results, and calculate the # pages needed
         num_res = soup_base.find(
-            "p", attrs={"class", "jobsCount"}).text.strip()
-        num_res = int(re.findall(r"(\d+)", num_res.replace(",", ""))[0])
+            'p', attrs={'class', 'jobsCount'}).text.strip()
+        num_res = int(re.findall(r'(\d+)', num_res.replace(',', ''))[0])
         log_info(
-            f"Found {num_res} glassdoor results for query=" f"{self.query}")
+            f'Found {num_res} glassdoor results for query=' f'{self.query}')
 
         pages = int(ceil(num_res / self.max_results_per_page))
 
@@ -152,16 +152,16 @@ class GlassDoorStatic(GlassDoorBase):
             else:
                 # gets partial url for next page
                 part_url = (
-                    soup_base.find("li", attrs={"class", "next"}).find(
-                        "a").get("href")
+                    soup_base.find('li', attrs={'class', 'next'}).find(
+                        'a').get('href')
                 )
                 # uses partial url to construct next page url
                 page_url = re.sub(
-                    r"_IP\d+\.",
-                    "_IP" + str(page) + ".",
-                    f"https://www.glassdoor."
+                    r'_IP\d+\.',
+                    '_IP' + str(page) + '.',
+                    f'https://www.glassdoor.'
                     f"{self.search_terms['region']['domain']}"
-                    f"{part_url}",
+                    f'{part_url}',
                 )
 
                 fts.append(  # append thread job future to futures list
@@ -178,69 +178,69 @@ class GlassDoorStatic(GlassDoorBase):
         # make a dict of job postings from the listing briefs
         for s in job_soup_list:
             # init dict to store scraped data
-            job = dict([(k, "") for k in MASTERLIST_HEADER])
+            job = dict([(k, '') for k in MASTERLIST_HEADER])
 
             # scrape the post data
-            job["status"] = "new"
+            job['status'] = 'new'
             try:
                 # jobs should at minimum have a title, company and location
-                job["title"] = (
-                    s.find("div", attrs={"class", "jobContainer"})
+                job['title'] = (
+                    s.find('div', attrs={'class', 'jobContainer'})
                     .find(
-                        "a",
-                        attrs={"class", "jobLink jobInfoItem jobTitle"},
+                        'a',
+                        attrs={'class', 'jobLink jobInfoItem jobTitle'},
                         recursive=False,
                     )
                     .text.strip()
                 )
-                job["company"] = s.find(
-                    "div", attrs={"class", "jobInfoItem jobEmpolyerName"}
+                job['company'] = s.find(
+                    'div', attrs={'class', 'jobInfoItem jobEmpolyerName'}
                 ).text.strip()
-                job["location"] = s.get("data-job-loc")
+                job['location'] = s.get('data-job-loc')
             except AttributeError:
                 continue
 
             # set blurb to none for now
-            job["blurb"] = ""
+            job['blurb'] = ''
 
             try:
-                labels = s.find_all("div", attrs={"class", "jobLabel"})
-                job["tags"] = "\n".join(
-                    [l.text.strip() for l in labels if l.text.strip() != "New"]
+                labels = s.find_all('div', attrs={'class', 'jobLabel'})
+                job['tags'] = '\n'.join(
+                    [l.text.strip() for l in labels if l.text.strip() != 'New']
                 )
             except AttributeError:
-                job["tags"] = ""
+                job['tags'] = ''
 
             try:
-                job["date"] = (
-                    s.find("div", attrs={"class", "jobLabels"})
-                    .find("span", attrs={"class", "jobLabel nowrap"})
+                job['date'] = (
+                    s.find('div', attrs={'class', 'jobLabels'})
+                    .find('span', attrs={'class', 'jobLabel nowrap'})
                     .text.strip()
                 )
             except AttributeError:
-                job["date"] = ""
+                job['date'] = ''
 
             try:
                 part_url = (
-                    s.find("div", attrs={"class", "logoWrap"}).find(
-                        "a").get("href")
+                    s.find('div', attrs={'class', 'logoWrap'}).find(
+                        'a').get('href')
                 )
-                job["id"] = s.get("data-id")
-                job["link"] = (
-                    f"https://www.glassdoor."
+                job['id'] = s.get('data-id')
+                job['link'] = (
+                    f'https://www.glassdoor.'
                     f"{self.search_terms['region']['domain']}"
-                    f"{part_url}"
+                    f'{part_url}'
                 )
 
             except (AttributeError, IndexError):
-                job["id"] = ""
-                job["link"] = ""
+                job['id'] = ''
+                job['link'] = ''
 
-            job["query"] = self.query
-            job["provider"] = self.provider
+            job['query'] = self.query
+            job['provider'] = self.provider
 
             # key by id
-            self.scrape_data[str(job["id"])] = job
+            self.scrape_data[str(job['id'])] = job
 
         # Do not change the order of the next three statements if you want date_filter to work
 
@@ -268,4 +268,4 @@ class GlassDoorStatic(GlassDoorBase):
 
             # end and print recorded time
             end = time()
-            print(f"{self.provider} scrape job took {(end - start):.3f}s")
+            print(f'{self.provider} scrape job took {(end - start):.3f}s')
