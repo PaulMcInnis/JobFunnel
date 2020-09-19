a9ffe022457b138b4e79d6d0d0a940c9448294ff
LilySu <LilySu@users.noreply.github.com>
2020-09-19 16:23:32 -0400

    refactored _get_search_url in indeed.py to organize UK and non-UK url string

--- check_dups.py
+++
@@ -1,7 +0,0 @@
-import fileinput
-seen = set() # set for fast O(1) amortized lookup
-for line in fileinput.FileInput('demo_job_search_results\demo_search.csv', inplace=1):
-    if line in seen: continue # skip duplicate
-
-    seen.add(line)
-    print(f'Duplicate Files{line,}') # standard output is now redirected to the file
