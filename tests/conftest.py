effef1ece33b6cbdac111a6e8c56c230f9f9ca16
thebigg <yunior.eury@gmail.com>
2020-10-14 20:05:19 -0500

    -Added tests for cli.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
-# FIXME
+import pytest
+import os
+
+
+# TODO: This should be a fixture. For now it is not because fixtures cannot be easily called as regular functions.
+def get_data_path():
+    """
+    Get the data path relative to this file. This is useful for when you want to test a single test file and not the
+    entire test suite.
+    :return:
+    """
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
