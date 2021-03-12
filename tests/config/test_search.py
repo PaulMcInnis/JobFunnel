fb78f56abe9fb188f074c052be9a09d442e6aba3
Marc <marchbnr@users.noreply.github.com>
2021-03-12 13:53:07 +0100

    Add a configuration test for the german locale

--- tests/config/test_search.py
+++ tests/config/test_search.py
@@ -33,6 +33,7 @@ def test_search_config_query_string(mocker, keywords, exp_query_str):
     (Locale.USA_ENGLISH, None, 'com'),
     (Locale.UK_ENGLISH, None, 'co.uk'),
     (Locale.FRANCE_FRENCH, None, 'fr'),
+    (Locale.GERMANY_GERMAN, None, 'de'),
     (Locale.USA_ENGLISH, 'xyz', 'xyz'),
     (None, None, None),
 ])
