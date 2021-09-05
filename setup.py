1824b3432f5359f45735832c904478cacf4c59e5
Ram Balachandran <janakiraman.balachandran@gmail.com>
2021-09-05 16:11:03 +0930

    upgraded requirements and setup for python 3.9

--- setup.py
+++ setup.py
@@ -6,25 +6,8 @@ from jobfunnel import __version__ as version
 
 description = 'Automated tool for scraping job postings.'
 url = 'https://github.com/PaulMcInnis/JobFunnel'
-requires = [
-    'beautifulsoup4>=4.6.3',
-    'lxml>=4.2.4',
-    'requests>=2.19.1',
-    'python-dateutil>=2.8.0',
-    'PyYAML>=5.1',
-    'scikit-learn>=0.21.2',
-    'nltk>=3.4.1',
-    'scipy>=1.4.1',
-    'pytest>=5.3.1',
-    'pytest-mock>=3.1.1',
-    'selenium>=3.141.0',
-    'webdriver-manager>=2.4.0',
-    'Cerberus>=1.3.2',
-    'tqdm>=4.47.0',
-    'flake8',
-    'pipenv',
-    'pytest-cov',
-]
+with open("requirements.txt") as req:
+    requires = [line.strip() for line in req if line and "#" not in line]
 here = Path(__file__).parent
 readme = (here / "readme.md").read_text()
 
@@ -39,7 +22,7 @@ setup(
     author_email='paulmcinnis99@gmail.com',
     url=url,
     license='MIT License',
-    python_requires='>=3.8.0',
+    python_requires='>=3.9.0',
     install_requires=requires,
     packages=find_packages(exclude=('tests', 'docs', 'images')),
     include_package_data=True,
@@ -47,6 +30,6 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
